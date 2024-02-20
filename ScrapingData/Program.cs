23a9d618c0da78d122a115d55e06d67a6bc5fbd1
Andrii Buteskul <buteskul5.6@gmail.com>
2024-02-21 01:48:35 +0200

    Init Scraper for equipments

--- ScrapingData/Program.cs
+++ ScrapingData/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HtmlAgilityPack;
+using ScrapingData;
+using System.Globalization;
+
+string url = "https://www.ilcats.ru/toyota/?function=getModels&market=EU";
+
+var web = new HtmlWeb();
+
+// parsing its HTML content
+
+var document = web.Load(url);
+
+
+var nodes = document.DocumentNode.
+    //SelectNodes("//*[@id=\"Body\"]/div/div[position() >= 1]/div[position()>=1]");
+    SelectNodes("//*[@id=\"Body\"]/div/div[position() >= 1]/div[position()>=1]");
+
+List<Model> models = new();
+
+// *** running ***
+
+//ScrapingData(models, nodes); 
+
+string toyotaEquipmentsUrl = "https://www.ilcats.ru/toyota/?function=getComplectations&market=EU&model=671440&startDate=198308&endDate=198903";
+string toyotaEquipmentsPath = "//*[@id=\"Body\"]/table[1]/tbody/tr[position()>1]"; 
+
+ScraperEquipment scraperEquipment = new(toyotaEquipmentsUrl, toyotaEquipmentsPath);
+
+var eq = scraperEquipment.GetScrapingData();
+
+foreach (var e in eq)
+{
+    Console.WriteLine($"{e.EquipmentCode} {e.Date} {e.Engine} {e.Body} {e.Grade} {e.GearShiftType}");
+}
+static void ScrapingData(List<Model> models, HtmlNodeCollection nodes)
+{
+    foreach (var node in nodes)
+    {
+        models.Add(
+            new Model()
+            {
+                ModelName = HtmlEntity.DeEntitize(node.SelectSingleNode("div[1]")?.InnerText),
+                ModelCode = Convert.ToInt32(HtmlEntity.DeEntitize(node.SelectSingleNode("div[2]/div/div[1]").InnerText)),
+                StartDate = GetDateFromString(HtmlEntity.DeEntitize(node.SelectSingleNode("div[2]/div/[2]").InnerText)),
+                EndDate = GetDateFromString(HtmlEntity.DeEntitize(node.SelectSingleNode("div[2]/div/[2]").InnerText)),
+                SpecificationName = HtmlEntity.DeEntitize(node.SelectSingleNode("div[2]/div/[3]").InnerText)
+                });
+    }
+    
+}
+
+static DateTime GetDateFromString(string str)
+{
+    var isValid = DateTime.TryParseExact(str, "MM.yyyy",
+                                        CultureInfo.CreateSpecificCulture("en-US"),
+                                        DateTimeStyles.None,
+                                        out DateTime date);
+    return isValid ? date : DateTime.MinValue;
+}
+
---
+++ ScrapingData/ScraperEquipment.cs
@@ -0,0 +1,47 @@
+﻿using HtmlAgilityPack;
+
+namespace ScrapingData
+{
+    internal class ScraperEquipment : IScraperData<Equipment>
+    {
+        private readonly HtmlWeb web;
+        private readonly HtmlDocument document;
+        private readonly HtmlNodeCollection nodes;
+        public List<Equipment> equipments;
+
+        private HtmlNode firstTable;
+        public ScraperEquipment(string url, string xPath)
+        {
+            web = new HtmlWeb();
+            document = web.Load(url);
+            equipments = new List<Equipment>();
+            firstTable = document.DocumentNode.Descendants("table").FirstOrDefault();
+        }
+        public List<Equipment> GetScrapingData()
+        {
+            foreach(var node in firstTable.Descendants("tr").Skip(1))
+            {
+                equipments.Add(
+                    new Equipment()
+                    {
+                        EquipmentCode = node.SelectSingleNode("td[1]").InnerText,
+                        Date = node.SelectSingleNode("td[2]").InnerText,
+                        Engine = node.SelectSingleNode("td[3]").InnerText,
+                        Body = node.SelectSingleNode("td[4]").InnerText,
+                        Grade = node.SelectSingleNode("td[5]").InnerText,
+                        Atm = node.SelectSingleNode("td[6]").InnerText,
+                        GearShiftType = node.SelectSingleNode("td[7]").InnerText,
+                        Cab = node.SelectSingleNode("td[8]").InnerText,
+                        TransmissionModel = node.SelectSingleNode("td[9]").InnerText,
+                        LoadingCapacity = node.SelectSingleNode("td[10]").InnerText,
+                        RearTire = node.SelectSingleNode("td[11]").InnerText,
+                        Destination = node.SelectSingleNode("td[12]").InnerText,
+                        FuelInduction = node.SelectSingleNode("td[13]").InnerText,
+                        BuildingCondition = node.SelectSingleNode("td[14]").InnerText
+                    });
+            }
+            return equipments;
+        }
+        
+    }
+}
